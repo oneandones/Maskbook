ad99dd101a268a1ea76f780e18db23d686da1711
guanbinrui <guanbinrui@dimension.im>
2020-06-10 13:12:25 +0800

    chore: remove unused UI components
    + comment
    + hide my wallet addr

--- src/plugins/Gitcoin/define.tsx
+++ src/plugins/Gitcoin/define.tsx
@@ -88,7 +88,7 @@ function Gitcoin(props: { post: string }) {
                     onClose={() => setOpen(false)}
                     title={title ?? 'A Gitcoin grant'}
                     description={description ?? ''}
-                    onDonate={async ({ comment, amount, selectedWallet, selectedToken, selectedTokenType }) => {
+                    onDonate={async ({ amount, selectedWallet, selectedToken, selectedTokenType }) => {
                         if (!address) {
                             return
                         }
@@ -101,7 +101,6 @@ function Gitcoin(props: { post: string }) {
                                 network: getNetworkSettings().networkType,
                                 token_type:
                                     selectedTokenType.type === 'eth' ? EthereumTokenType.eth : EthereumTokenType.erc20,
-                                comment,
                                 token: selectedToken,
                             })
                         } catch (e) {
